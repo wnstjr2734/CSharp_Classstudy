f377b9fa96371dbfa02aea79d09c7a765dc2fe55
이준석 <wnstjr2734@naver.com>
2022-11-03 21:53:43 +0900

    ClassStudy 폴더 추가
    Class 스터디 한 코드 추가

---
+++ ClassStudy/ClassStudy/Brick.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace StaticTest
+{
+    //이러한 클래스의 단점은 클래스의 필드값이 고정되어있어서 변경이 힘들다.
+    class Brick
+    {
+        //필드(데이터)
+        private int width;  //private는 현재 클래스 내에서만 쓰이게한다. 외부에서 엑세스가 안된다.
+        private int height;
+        private int depth;
+        private Color color;
+        private int volume;
+
+        //생성자(Constructor)
+        public Brick()
+        {
+            width = 10;
+            height = 10;
+            depth = 5;
+            color = Color.Red;
+        }
+
+        //위의 클래스를 보완하기 위해 파라미터를 설정
+        public Brick(int w, int h, int d, Color c)
+        {
+            //위의 매개변수 지정
+            this.width = w;
+            this.height = h;
+            this.depth = d;
+            this.color = c;
+        }
+
+        //속성(property)
+        public int Width
+        {
+            //get은 읽기전용이다.
+            get { return width; }
+            //set을 쓰게 되면 value 값을 집어넣을수 있게 만든다.
+            set { width = value; }
+        }
+
+        public Color Color
+        {
+            get { return Color; }
+        }
+
+        public int Volume
+        {
+            get { return width * height * depth; }
+            set { volume = value; }
+        }
+    }
+}
---
+++ ClassStudy/ClassStudy/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+
+namespace StaticTest
+{
+    class Program
+    {
+        static void Main(string[] Args)
+        {
+            //새로운 Brick 클래스를 담는 br이라는 변수를 만든다(생성자)
+            Brick br = new Brick();
+            Brick br2 = new Brick(50, 100, 20, Color.Gray);
+            Brick br3 = new Brick();
+
+            //set을 설정하여 새로운 값을 집어넣을수 있게 한다
+            int w = br2.Width;
+            w = 100;
+
+            int volume = br3.Volume;
+            int v = volume;
+            v = 2 * 2 * 3;
+
+            Console.WriteLine($"br2의 width는 {w}이다.");
+            Console.WriteLine(v);
+        }
+    }
+}
